930d3d47fb08b2f1a9986bd7cd42165fbd652713
liyubin <lixin58688@163.com>
2018-09-19 21:23:19 +0800

    新增flink-cep依赖，新增分割选择流、CEP温度预警

---
+++ src/main/java/cep/TemperatureAlertDemo.java
@@ -0,0 +1,59 @@
+package cep;
+
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternSelectFunction;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.IterativeCondition;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.List;
+import java.util.Map;
+
+public class TemperatureAlertDemo {
+
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // DataStream : source
+        DataStream<TemperatureEvent> inputEventStream = env.fromElements(new TemperatureEvent("xyz",22.0),
+                new TemperatureEvent("xyz",20.1), new TemperatureEvent("xyz",21.1),
+                new TemperatureEvent("xyz",22.2), new TemperatureEvent("xyz",22.1),
+                new TemperatureEvent("xyz",22.3), new TemperatureEvent("xyz",22.1),
+                new TemperatureEvent("xyz",22.4), new TemperatureEvent("xyz",22.7),
+                new TemperatureEvent("xyz",27.0), new TemperatureEvent("xyz",30.0));
+
+        // 定义Pattern，检查10秒钟内温度是否高于26度
+        Pattern<TemperatureEvent,?> warningPattern = Pattern.<TemperatureEvent>begin("start")
+                .subtype(TemperatureEvent.class)
+                .where(new IterativeCondition<TemperatureEvent>() {
+                    private static final long serialVersionUID = 1L;
+
+                    public boolean filter(TemperatureEvent value, Context<TemperatureEvent> ctx) throws Exception {
+                        if(value.getTemperature() >= 26.0){
+                            return true;
+                        }
+                        return false;
+                    }
+                })
+                .within(Time.seconds(10));
+
+        //匹配pattern并select事件,符合条件的发生警告，即输出
+        DataStream<Alert> patternStream = CEP.pattern(inputEventStream, warningPattern)
+                .select(new PatternSelectFunction<TemperatureEvent, Alert>() {
+                    private static final long serialVersionUID = 1L;
+
+                    public Alert select(Map<String, List<TemperatureEvent>> event) throws Exception {
+                        System.out.println(event.size());
+                        TemperatureEvent e = event.get("start").get(0);
+                        return new Alert("Temperature Rise Detected: " + e.getTemperature() + " on machine name: " + e.getMachineName());
+                    }
+                });
+
+        patternStream.print();
+
+        env.execute("CEP on Temperature Sensor");
+    }
+}
