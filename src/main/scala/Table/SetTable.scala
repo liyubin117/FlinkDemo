5957a6aa8e282ba4c52e79ddc8f3717df39e2e25
liyubin <lixin58688@163.com>
2018-09-19 11:38:19 +0800

    新增Scala DataStream Table使用

--- src/main/scala/Table/SetTable.scala
+++ src/main/scala/Table/SetTable.scala
@@ -18,16 +18,19 @@ object SetTable extends App{
   )
   //生成DataSet
   val personSet: DataSet[Person] = input.map(_.split(",")).map(x => Person(x(0).toInt, x(1), x(2).toInt))
-//  tableEnv.registerDataSet("person",personSet,'id, 'name, 'age)
-  tableEnv.registerDataSet("person", personSet)
+
 
   //指定表程序
 //  val counts: Table = tableEnv.fromDataSet(personSet).filter("age>=26").groupBy("age").select("age,id.count() as cnt")
+
+//  tableEnv.registerDataSet("person",personSet,'id, 'name, 'age)
+  tableEnv.registerDataSet("person", personSet)
   val counts: Table = tableEnv.sqlQuery("select age,count(distinct id) from person where age>=26 group by age")
+
   //结果转化为DataSet
   val result: DataSet[Result] = tableEnv.toDataSet[Result](counts)
   result.print()
 }
 
 case class Person(id:Integer, name:String, age:Integer)
-case class Result(age:Int, cnt:Long)
+case class Result(age:Integer, cnt:Long)
--- src/main/scala/Table/StreamTable.scala
+++ src/main/scala/Table/StreamTable.scala
@@ -1,20 +1,26 @@
-//package Table
-//
-//import java.beans.Expression
-//
-//import org.apache.flink.streaming.api.scala._
-//import org.apache.flink.table.api.TableEnvironment
-//
-//object StreamTable extends App{
-//  //注册datastream
-//  val env = StreamExecutionEnvironment.getExecutionEnvironment
-//  val tableEnv = TableEnvironment.getTableEnvironment(env)
-//
-//  val host="spark"
-//  val port=9888
-//  val input = env.socketTextStream(host, port)
-//  val person = input.map(_.split(",")).map(x => Person(x(0).toInt, x(1), x(2).toInt))
-//  tableEnv.registerDataStream("person", person)
-//}
-//
-//case class Person(id:Int, name:String, age:Int)
+package Table
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.api.TableEnvironment
+
+object StreamTable extends App{
+  //注册datastream
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val tableEnv = TableEnvironment.getTableEnvironment(env)
+
+  val host="spark"
+  val port=9888
+  val input = env.socketTextStream(host, port)
+  val person: DataStream[Person] = input.map(_.split(",")).filter(_.length==3).map(x => Person(x(0).toInt, x(1), x(2).toInt))
+
+//  val counts = tableEnv.fromDataStream(person).filter("age>=26").groupBy("age").select("age,id.count() as cnt")
+
+  tableEnv.registerDataStream("person",person)
+  val counts = tableEnv.sqlQuery("select age,count(id) as cnt from person where age>=26 group by age")
+
+  counts.printSchema()
+  val result = tableEnv.toRetractStream[Result](counts)
+  result.print()
+  env.execute()
+}
