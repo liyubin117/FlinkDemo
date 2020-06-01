a7733ce3cad782c0afd6c25b9726ae8d9bb51ff8
Yubin Li <lixin58688@163.com>
2020-06-01 16:33:02 +0800

    add

---
+++ src/main/java/hadoop/KerberosDemo.java
@@ -0,0 +1,34 @@
+package hadoop;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class KerberosDemo {
+    private static final Logger log = LoggerFactory.getLogger(KerberosDemo.class);
+
+    @Test
+    public void setUp() throws IOException {
+        initKerberos("config/krb5.conf",
+                "liyubin",
+                "config/liyubin.keytab");
+    }
+
+
+    public static void initKerberos(String krb5ConfPath, String principal, String keytab) throws IOException {
+        System.setProperty("java.security.krb5.conf", krb5ConfPath);
+        System.setProperty("sun.security.krb5.debug", "true");
+        org.apache.hadoop.conf.Configuration conf = new Configuration();
+        conf.set("hadoop.security.authentication", "kerberos");
+        UserGroupInformation.setConfiguration(conf);
+        UserGroupInformation.loginUserFromKeytab(principal, keytab);
+        log.warn("getting connection from kudu with kerberos");
+        log.warn("----------current user: " + UserGroupInformation.getCurrentUser() + "----------");
+        log.warn("----------login user: " + UserGroupInformation.getLoginUser() + "----------");
+    }
+}
---
+++ src/main/java/hadoop/UDFDecoderUrl.java
@@ -0,0 +1,79 @@
+package hadoop;
+
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.net.URLDecoder;
+
+@Description(name = "decoder_url", value = "_FUNC_(url [,code][,count]) - decoder a URL from a String for count times using code as encoding scheme ", extended = ""
+    + "if count is not given ,the url will be decoderd for 2 time,"
+    + "if code is not given ,GBK is used")
+public class UDFDecoderUrl extends UDF {
+  private String url = null;
+  private int times = 2;
+  private String code = "GBK";
+
+  public UDFDecoderUrl() {
+  }
+
+  public String evaluate(String urlStr, String srcCode, int count) {
+    if (urlStr == null) {
+      return null;
+    }
+    if (count <= 0) {
+      return urlStr;
+    }
+    if (srcCode != null) {
+      code = srcCode;
+    }
+    url = urlStr;
+    times = count;
+    for (int i = 0; i < times; i++) {
+      url = decoder(url, code);
+    }
+    return url;
+  }
+
+  public String evaluate(String urlStr, String srcCode) {
+    if (urlStr == null) {
+      return null;
+    }
+    url = urlStr;
+    code = srcCode;
+    return evaluate(url, code,times);
+  }
+
+  public String evaluate(String urlStr, int count) {
+    if (urlStr == null) {
+      return null;
+    }
+    if (count <= 0) {
+      return urlStr;
+    }
+    url = urlStr;
+    times = count;
+
+    return evaluate(url, code,times);
+  }
+
+  public String evaluate(String urlStr) {
+    if (urlStr == null) {
+      return null;
+    }
+    url = urlStr;
+    return evaluate(url, code,times);
+  }
+
+  private String decoder(String urlStr, String code) {
+    if (urlStr == null || code == null) {
+      return null;
+    }
+    try {
+      urlStr = URLDecoder.decode(urlStr, code);
+    } catch (Exception e) {
+      return null;
+    }
+    return urlStr;
+  }
+}
