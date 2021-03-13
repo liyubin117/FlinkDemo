c3dafc541af51fc0709690c615d031ac0646f7c3
Yubin Li <lixin58688@163.com>
2021-03-13 12:22:24 +0800

    update flink sql

--- src/main/scala/sql/TableDemo.scala
+++ src/main/scala/sql/TableDemo.scala
@@ -1,23 +1,44 @@
 package sql
-import org.apache.flink.table.api.DataTypes
-import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
+import org.apache.flink.table.api._
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, OldCsv, Schema}
 import org.apache.flink.api.scala._
 import org.apache.flink.table.api.bridge.scala._
 import sql.EnvDemo.{bsTableEnv, env}
 object TableDemo {
   def main(args: Array[String]): Unit = {
+    //csv table
     val filePath = "file/person.csv"
+    val schema = new Schema()  //定义字段
+      .field("id", DataTypes.BIGINT())
+      .field("name", DataTypes.STRING())
+      .field("age", DataTypes.BIGINT())
+
     bsTableEnv.connect(new FileSystem().path(filePath))
       .withFormat(new OldCsv) //定义文件格式
-      .withSchema(new Schema()  //定义字段
-        .field("id", DataTypes.BIGINT())
-        .field("name", DataTypes.STRING())
-        .field("age", DataTypes.BIGINT())
-      )
+      .withSchema(schema)
       .createTemporaryTable("person")
     val personTable = bsTableEnv.from("person")
-    val personStream = personTable.toAppendStream[(Long, String, Long)]
-    personStream.print()
+          .filter($("age") > 20)
+          .select($("id"), $("name"))
+    val personStream = personTable.toAppendStream[(Long, String)]
+    personStream.print("result")
+
+    bsTableEnv.from("person")
+      .filter($("age") > 25)
+      .groupBy($("id"))
+      .select($("id"), $("name").count.as("cnt"))
+      .toRetractStream[(Long, Long)]
+      .print("aggre result")
+
+    //kafka table
+    bsTableEnv.connect(new Kafka()
+      .version("0.11")
+      .topic("test")
+      .property("zookeeper.connect", "localhost:2181")
+      .property("boostrap.server", "localhost:9092")
+    ).withFormat(new Csv())
+      .withSchema(schema)
+      .createTemporaryTable("personKafka")
 
     env.execute("Table Demo")
   }
