4328ffc08f1e2f65c9ce45c322a42c9f6c6ababd
Yubin Li <lixin58688@163.com>
2022-09-01 19:39:12 +0800

    add rocketmq e2e test

---
+++ lyb-e2e-tests/src/test/java/org/lyb/rocketmq/RocketmqE2eTest.java
@@ -0,0 +1,53 @@
+package org.lyb.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.junit.Before;
+import org.junit.Test;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import static org.lyb.rocketmq.Utils.generateConsumer;
+import static org.lyb.rocketmq.Utils.generateProducer;
+
+public class RocketmqE2eTest {
+    private static final String DOCKER_IMAGE_NAME = "apache/rocketmq:4.9.4";
+    private static GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse(DOCKER_IMAGE_NAME)).withExposedPorts(9876);
+    DockerComposeContainer composeContainer = new DockerComposeContainer<>(
+           new File(RocketmqE2eTest.class.getClassLoader()
+                                                .getResource("docker-compose.yaml")
+                                                .toURI()));
+
+    public static final String TOPIC = "topic1";
+    public static final String TAG = "tag1";
+    private String nameSrvAddress;
+
+    public RocketmqE2eTest() throws URISyntaxException {
+    }
+
+    @Before
+    public void setUp() {
+        container.start();
+        nameSrvAddress = container.getHost() + ":" + container.getFirstMappedPort();
+    }
+
+    @Test
+    public void test1() throws MQClientException, MQBrokerException, RemotingException, InterruptedException {
+
+        DefaultMQProducer producer = generateProducer("group1", nameSrvAddress);
+        producer.send(new Message(TOPIC, TAG, "a message".getBytes()));
+        producer.shutdown();
+        DefaultMQPushConsumer consumer = generateConsumer("group1", nameSrvAddress, ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET, TOPIC, TAG);
+
+    }
+
+}
