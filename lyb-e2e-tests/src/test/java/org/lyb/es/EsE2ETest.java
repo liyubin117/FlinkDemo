5707b816286cb256950ce267e782c0c0e682eb88
Yubin Li <lixin58688@163.com>
2022-09-23 15:16:17 +0800

    e2e tests: es

---
+++ lyb-e2e-tests/src/test/java/org/lyb/es/EsE2ETest.java
@@ -0,0 +1,27 @@
+package org.lyb.es;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+public class EsE2ETest {
+    private ElasticsearchContainer elasticsearchContainer;
+
+    @Before
+    public void before() {
+        elasticsearchContainer =
+                new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.9.2");
+        elasticsearchContainer.start();
+    }
+
+    @After
+    public void after() {
+        elasticsearchContainer.stop();
+    }
+
+    @Test
+    public void test1() {
+
+    }
+}
