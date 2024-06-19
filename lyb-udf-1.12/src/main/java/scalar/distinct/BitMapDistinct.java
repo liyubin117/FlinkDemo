0652f5c4dc42564bcb83a3598df4ebc88399598f
Yubin Li <lixin58688@163.com>
2024-06-20 00:43:43 +0800

    add four distinct udf via MapState/HLL/BloomFilter/BitMap

---
+++ lyb-udf-1.12/src/main/java/scalar/distinct/BitMapDistinct.java
@@ -0,0 +1,28 @@
+package scalar.distinct;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.roaringbitmap.longlong.Roaring64NavigableMap;
+
+public class BitMapDistinct implements AggregateFunction<Long, Roaring64NavigableMap,Long> {
+
+    @Override
+    public Roaring64NavigableMap createAccumulator() {
+        return new Roaring64NavigableMap();
+    }
+
+    @Override
+    public Roaring64NavigableMap add(Long value, Roaring64NavigableMap accumulator) {
+        accumulator.add(value);
+        return accumulator;
+    }
+
+    @Override
+    public Long getResult(Roaring64NavigableMap accumulator) {
+        return accumulator.getLongCardinality();
+    }
+
+    @Override
+    public Roaring64NavigableMap merge(Roaring64NavigableMap a, Roaring64NavigableMap b) {
+        return null;
+    }
+}
